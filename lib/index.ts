ca1064e1d139eb9e7e3164a1aefde6d143cc1571
zhoulixiang <18366276315@163.com>
2026-01-27 16:19:16 +0800

    feat: test bot

--- lib/index.ts
+++ lib/index.ts
@@ -14,6 +14,7 @@ import { guessEditor } from './guess';
 import { getEnvVariable } from './utils';
 import { EDITORS_OPEN_MAP } from './editor-info/mac';
 
+console.log('test')
 function isTerminalEditor(editor: string) {
   switch (editor) {
     case 'vim':
